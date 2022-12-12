b87ed15f6a1ceac927e113be813e706f1bf787a0
nada1221 <nada931221@gmail.com>
2022-12-13 03:08:50 +0900

    📦 chore : 터미널 정리 및 버튼 삭제

--- pages/vscodestatesNew.js
+++ pages/vscodestatesNew.js
@@ -2,18 +2,11 @@ import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import OnSideBar from '../components/VSC/OnSidebar';
 import BottomLine from '../components/VSC/BottomLine';
-import TerminalSpace from '../components/VSC/Terminal';
 import FolderLine from '../components/VSC/LeftFolderLine';
 import MainInput from '../components/VSC/MainInput';
 import NewTabTab from '../components/VSC/NewLetterTab';
-import ButtonCP from '../components/VSC/Button';
 import axios from 'axios';
 
-const body = styled.div`
-  display: flex;
-  flex-direction: row;
-  background-color: #1e1e1e;
-`;
 const Body = styled.div`
   display: flex;
   flex-direction: row;
@@ -26,6 +19,11 @@ const VscContain = styled.div`
   width: 100%;
   height: 100vh;
 `;
+
+const WelcomeText = styled.div`
+  margin: 76px 0 0 10px;
+`;
+
 const NewLetter = () => {
   const [data, setData] = useState();
   const [content, setContent] = useState();
@@ -79,14 +77,12 @@ const NewLetter = () => {
         id={id}
       />
       <VscContain>
-        <NewTabTab />
+        <NewTabTab contentsData={data && data} id={id} />
         {content ? (
           <MainInput data={content && content} />
         ) : (
-          <div>편지를 기다리고 있어요!</div>
+          <WelcomeText>{`← 보고싶은 롤링페이퍼를 클릭해보세요!`}</WelcomeText>
         )}
-        <ButtonCP />
-        <TerminalSpace />
         <BottomLine />
       </VscContain>
     </Body>
