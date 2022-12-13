1763924a656dc3f71fcd1aa151f0728cb5e06734
서재홍 <tkwh1133@gmail.com>
2022-12-13 15:24:07 +0900

    ✨ feat :  Link 나머지 추가

--- components/Main/Render.js
+++ components/Main/Render.js
@@ -56,8 +56,12 @@ const Render = () => {
         <Link href={'/maekjoo'}>
           <MainButton></MainButton>
         </Link>
-        <MainButton></MainButton>
-        <MainButton></MainButton>
+        <Link href={'/progamers'}>
+          <MainButton></MainButton>
+        </Link>
+        <Link href={'/vscodestates'}>
+          <MainButton></MainButton>
+        </Link>
       </MainDiv>
     </MainArea>
   );
