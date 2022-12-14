b9c469256012be58715aaa426b5fdcd083c21b01
billy5982 <kuu0404@naver.com>
2022-12-14 18:16:49 +0900

    🐛  fix : progamers 예외 처리 완료

--- pages/progamers.js
+++ pages/progamers.js
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Header from '../components/Progamers/Header';
 import styled from 'styled-components';
 import Progamers_Main from '../components/Progamers/Main';
 import Progamers_Footer from '../components/Progamers/Footer';
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/router';
 
 const Progamers = () => {
+  const { data: session } = useSession();
+
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!session) {
+      alert('로그인이 필요한 서비스입니다.');
+      router.push('/');
+    }
+  }, [session]);
   return (
     <>
-      <Progamers_Container>
-        <Header> </Header>
-        <Progamers_Main> </Progamers_Main>
-        <Progamers_Footer> </Progamers_Footer>
-      </Progamers_Container>
+      {session && (
+        <Progamers_Container>
+          <Header> </Header>
+          <Progamers_Main> </Progamers_Main>
+          <Progamers_Footer> </Progamers_Footer>
+        </Progamers_Container>
+      )}
     </>
   );
 };
